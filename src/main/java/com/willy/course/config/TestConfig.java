cfb4717516a62dafb78b0a6630a5d0294dc552d7
Willy Gutsche <willy.gutsche@unifesp.br>
2023-02-11 13:25:23 -0300

    Payment entity and one to one association with order

--- src/main/java/com/willy/course/config/TestConfig.java
+++ src/main/java/com/willy/course/config/TestConfig.java
@@ -35,11 +35,15 @@ public class TestConfig implements CommandLineRunner {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT,  user1);
-        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID,user2);
-        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED,user1);
-
         userRepository.saveAll(Arrays.asList(user1, user2));
+
+        Order order1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.WAITING_PAYMENT,  user1);
+        Order order2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.PAID, user2);
+        Order order3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.CANCELED, user1);
+
+        Payment payment2 = new Payment(null, Instant.parse("2019-07-21T05:42:10Z"), order2);
+        order2.setPayment(payment2);
+
         orderRepository.saveAll(Arrays.asList(order1, order2, order3));
 
         Category category1 = new Category(null, "Electronics");
